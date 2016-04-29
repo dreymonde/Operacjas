df5e41386ac442ef3491ee95c660880348a89836
Oleg Dreyman <dreymonde@me.com>
2016-04-29 20:09:45 +0300

    Initial Commit

---
+++ Operations/Operations.h
@@ -0,0 +1,19 @@
+//
+//  Operations.h
+//  Operations
+//
+//  Created by Oleg Dreyman on 29.04.16.
+//  Copyright © 2016 AdvancedOperations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Operations.
+FOUNDATION_EXPORT double OperationsVersionNumber;
+
+//! Project version string for Operations.
+FOUNDATION_EXPORT const unsigned char OperationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Operations/PublicHeader.h>
+
+
