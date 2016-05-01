4f105e71a35f170c33337aebdca693fd64f339ed
Oleg Dreyman <dreymonde@me.com>
2016-05-01 15:55:08 +0300

    Core Apple logic

--- Resources/Operations.h
+++ Resources/Operations.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 AdvancedOperations. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Operations.
 FOUNDATION_EXPORT double OperationsVersionNumber;
